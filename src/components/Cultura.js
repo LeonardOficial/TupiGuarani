36f4d1e747cf20db482efc885c9858376162966f
Leonardo Amaral <leonardoficial@hotmail.com>
2017-08-15 10:51:54 -0300

    foo

--- src/components/Cultura.js
+++ src/components/Cultura.js
@@ -19,11 +19,7 @@ export default class Cultura extends Component {
             <font color="#e9c108">Musica</font> e <font color="#e9c108">dança</font> também tem grande espaço de apreciação entre os índios, os jovens em sua maioria sabem
             tocar <font color="#e9c108">violão</font> e <font color="#e9c108">rabeca</font> (um tipo de violino rústico), além de praticarem o <font color="#e9c108">canto coral</font>, preservam também em 
             seus costumes danças que possuem algumas especificidades, umas são apenas para meninos, outras somente para
-            meninas e outras mais para os adultos. Há quinhentos anos os Guaranis têm enfrentado o desafio de sobreviver de acordo 
-            com suas tradições, interagindo com a sociedade brasileira. Vêm selecionando e incorporando às suas tradições e valores as 
-            novas necessidades e conhecimentos advindos dessa relação. O jeito de ser guarani, “nhandereko”, e os costumes deste simples 
-            e simpático povo pode ser conhecido através de visitas guiadas a aldeia, que são agendadas com direito a
-            um mergulho na bela <font color="#28afc8">cachoeira do bracuì</font>.
+            meninas e outras mais para os adultos.
           </p>
         </div>
         
